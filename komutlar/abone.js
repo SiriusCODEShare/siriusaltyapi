7bec08fae0e86ac65fdfccab5415ffd065062d41
Glitch (v12-kayit-altyapi) <none>
2020-12-30 16:36:59 +0000

    🍱😎 Checkpoint ./komutlar/abone.js:23040536/2

--- komutlar/abone.js
+++ komutlar/abone.js
@@ -4,7 +4,7 @@ exports.run = async (client, message, args) => {
   
   const codeworkabone = await db.fetch(`abonek_${message.guild.id}`)
   
-    if(codeworkabone == null) return message.channel.send('');
+  if(codeworkabone == null) return message.channel.send('');
   if (message.channel.id !== codeworkabone) return message.channel.send(`Bu Komutu Sadece ${codeworkabone} Kanalında Kullanabilirsiniz!`);
   if (codeworkabone == true) return; 
   if (codeworkabone == false) return message.channel.send(`Bu Sunucuda Abone Sistemi Aktif Edilmemiş.`);
